f158195f7dfcfb9ed56cff1ebfa1ca5e5211fb4c
QamarTheCoder <qamarthecoder69@gmail.com>
2024-06-15 13:30:12 +0500

    done

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 //add new feature - agar tujy
 //add new feature - agar -suiiiii
-=======
 //add new feature - agar mujay ho gaya kuch
 //add new feature - agar tujy -sui
->>>>>>> feature
+
